0d6a5ac2117c014dfb85aa01656bf5547b87cb5d
Anders Østby <anders.ostby@nav.no>
2020-06-16 13:45:22 +0200

    Tester mapping av tilstand for sykmeldinger
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>

--- src/main/kotlin/no/nav/helse/sporbar/VedtaksperiodeMediator.kt
+++ src/main/kotlin/no/nav/helse/sporbar/VedtaksperiodeMediator.kt
@@ -1,24 +1,17 @@
 package no.nav.helse.sporbar
 
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 private val log: Logger = LoggerFactory.getLogger("sporbar")
-private val objectMapper = jacksonObjectMapper()
-    .registerModule(JavaTimeModule())
-    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-
 
 internal class VedtaksperiodeMediator(
     private val vedtaksperiodeDao: VedtaksperiodeDao,
     private val producer: KafkaProducer<String, VedtaksperiodeDto>
 ) {
-    fun vedtaksperiodeEndret(vedtaksperiodeEndret: VedtaksperiodeEndret) {
+    internal fun vedtaksperiodeEndret(vedtaksperiodeEndret: VedtaksperiodeEndret) {
         vedtaksperiodeDao.opprett(
             fnr = vedtaksperiodeEndret.fnr,
             orgnummer = vedtaksperiodeEndret.orgnummer,
@@ -28,8 +21,11 @@ internal class VedtaksperiodeMediator(
             tilstand = vedtaksperiodeEndret.tilstand
         )
 
-        val dto = vedtaksperiodeDao.finn(vedtaksperiodeEndret.vedtaksperiodeId).toDto()
-        producer.send(ProducerRecord("topic", "fnr", dto))
+        producer.send(ProducerRecord(
+            "topic",
+            "fnr",
+            vedtaksperiodeDao.finn(vedtaksperiodeEndret.vedtaksperiodeId).toDto()
+        ))
     }
 }
 
@@ -41,7 +37,7 @@ internal fun Vedtaksperiode.toDto() = VedtaksperiodeDto(
     tilstand = oversettTilstand(tilstand)
 )
 
-internal fun oversettTilstand(tilstand: Vedtaksperiode.Tilstand) = when(tilstand) {
+internal fun oversettTilstand(tilstand: Vedtaksperiode.Tilstand) = when (tilstand) {
     Vedtaksperiode.Tilstand.MOTTATT_SYKMELDING_FERDIG_GAP -> VedtaksperiodeDto.TilstandDto.AvventerDokumentasjon
     else -> VedtaksperiodeDto.TilstandDto.AvventerDokumentasjon
 }
---
+++ src/test/kotlin/no/nav/helse/sporbar/VedtaksperiodeMediatorTest.kt
@@ -0,0 +1,74 @@
+package no.nav.helse.sporbar
+
+import io.mockk.CapturingSlot
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.util.*
+
+private const val FNR = ""
+private const val ORGNUMMER = ""
+
+internal class VedtaksperiodeMediatorTest {
+
+    private val vedtaksperiodeDao = mockk<VedtaksperiodeDao>(relaxed = true)
+    private val producer = mockk<KafkaProducer<String, VedtaksperiodeDto>>(relaxed = true)
+    private val vedtaksperiodeMediator = VedtaksperiodeMediator(vedtaksperiodeDao, producer)
+
+    private val sykmeldingDokument = Dokument(UUID.randomUUID(), Dokument.Type.Sykmelding)
+
+    @Test
+    fun `sendt sykmelding uten forlengelse`() {
+        every { vedtaksperiodeDao.finn(any()) } returns Vedtaksperiode(
+            fnr = FNR,
+            orgnummer = ORGNUMMER,
+            vedtak = null,
+            dokumenter = listOf(sykmeldingDokument),
+            tilstand = Vedtaksperiode.Tilstand.MOTTATT_SYKMELDING_FERDIG_GAP
+        )
+
+        vedtaksperiodeMediator.vedtaksperiodeEndret(VedtaksperiodeEndret(
+            fnr = FNR,
+            orgnummer = ORGNUMMER,
+            vedtaksperiodeId = UUID.randomUUID(),
+            hendelseIder = emptyList(),
+            timestamp = LocalDateTime.now(),
+            tilstand = Vedtaksperiode.Tilstand.MOTTATT_SYKMELDING_FERDIG_GAP
+        ))
+
+        val slot = CapturingSlot<ProducerRecord<String,VedtaksperiodeDto>>()
+        verify { producer.send(capture(slot)) }
+
+        assertEquals(VedtaksperiodeDto.TilstandDto.AvventerDokumentasjon, slot.captured.value().tilstand)
+    }
+
+    @Test
+    fun `sendt sykmelding med forlengelse`() {
+        every { vedtaksperiodeDao.finn(any()) } returns Vedtaksperiode(
+            fnr = FNR,
+            orgnummer = ORGNUMMER,
+            vedtak = null,
+            dokumenter = listOf(sykmeldingDokument),
+            tilstand = Vedtaksperiode.Tilstand.MOTTATT_SYKMELDING_FERDIG_GAP
+        )
+
+        vedtaksperiodeMediator.vedtaksperiodeEndret(VedtaksperiodeEndret(
+            fnr = FNR,
+            orgnummer = ORGNUMMER,
+            vedtaksperiodeId = UUID.randomUUID(),
+            hendelseIder = emptyList(),
+            timestamp = LocalDateTime.now(),
+            tilstand = Vedtaksperiode.Tilstand.MOTTATT_SYKMELDING_FERDIG_FORLENGELSE
+        ))
+
+        val slot = CapturingSlot<ProducerRecord<String,VedtaksperiodeDto>>()
+        verify { producer.send(capture(slot)) }
+
+        assertEquals(VedtaksperiodeDto.TilstandDto.AvventerDokumentasjon, slot.captured.value().tilstand)
+    }
+}
