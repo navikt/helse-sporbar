f3f93ba1217bf53cbd15ae07dde9d70d44de7b01
Anders Østby <anders.ostby@nav.no>
2020-06-19 10:57:15 +0200

    Logger feilede meldinger til sikker logg
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>

--- src/main/kotlin/no/nav/helse/sporbar/VedtaksperiodeEndretRiver.kt
+++ src/main/kotlin/no/nav/helse/sporbar/VedtaksperiodeEndretRiver.kt
@@ -7,9 +7,10 @@ import no.nav.helse.rapids_rivers.asLocalDateTime
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
-import java.util.UUID
+import java.util.*
 
 private val log: Logger = LoggerFactory.getLogger("sporbar")
+private val sikkerLog: Logger = LoggerFactory.getLogger("tjenestekall")
 
 internal class VedtaksperiodeEndretRiver(
     rapidsConnection: RapidsConnection,
@@ -33,18 +34,23 @@ internal class VedtaksperiodeEndretRiver(
     }
 
     override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
-        val vedtaksperiodeId = UUID.fromString(packet["vedtaksperiodeId"].asText())
-        vedtaksperiodeMediator.vedtaksperiodeEndret(
-            VedtaksperiodeEndret(
-                fnr = packet["fødselsnummer"].asText(),
-                orgnummer = packet["organisasjonsnummer"].asText(),
-                vedtaksperiodeId = vedtaksperiodeId,
-                hendelseIder = packet["hendelser"].map { UUID.fromString(it.asText()) },
-                timestamp = packet["@opprettet"].asLocalDateTime(),
-                tilstand = enumValueOf(packet["gjeldendeTilstand"].asText())
+        try {
+            val vedtaksperiodeId = UUID.fromString(packet["vedtaksperiodeId"].asText())
+            vedtaksperiodeMediator.vedtaksperiodeEndret(
+                VedtaksperiodeEndret(
+                    fnr = packet["fødselsnummer"].asText(),
+                    orgnummer = packet["organisasjonsnummer"].asText(),
+                    vedtaksperiodeId = vedtaksperiodeId,
+                    hendelseIder = packet["hendelser"].map { UUID.fromString(it.asText()) },
+                    timestamp = packet["@opprettet"].asLocalDateTime(),
+                    tilstand = enumValueOf(packet["gjeldendeTilstand"].asText())
+                )
             )
-        )
-        log.info("Vedtaksperiode $vedtaksperiodeId upserted")
+            log.info("Vedtaksperiode $vedtaksperiodeId upserted")
+        } catch (e: Exception) {
+            sikkerLog.error("Feil ved behandling av melding: {}", packet.toJson(), e)
+            throw e
+        }
     }
 }
 
