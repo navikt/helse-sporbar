ad26eb6ac792505e2122156cbb73b280b4eb40c2
Knut Nygaard <knut.nygaard@nav.no>
2020-11-11 13:51:45 +0100

    Fjerner featuretoggle rundt publisering av annulleringsevents

--- src/main/kotlin/no/nav/helse/sporbar/App.kt
+++ src/main/kotlin/no/nav/helse/sporbar/App.kt
@@ -45,16 +45,13 @@ fun launchApplication(env: Environment) {
         dokumentDao = dokumentDao,
         producer = producer
     )
-    val miljøstyrtFeatureToggle = MiljøstyrtFeatureToggle(env.raw)
 
     RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(env.raw))
         .build().apply {
             NyttDokumentRiver(this, dokumentDao)
             VedtaksperiodeEndretRiver(this, mediator)
             UtbetaltRiver(this, mediator)
-            if(miljøstyrtFeatureToggle.annullering()) {
-                AnnulleringRiver(this, producer)
-            }
+            AnnulleringRiver(this, producer)
             start()
         }
 }
--- src/main/kotlin/no/nav/helse/sporbar/MiljøstyrtFeatureToggle.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.helse.sporbar
-
-internal class MiljøstyrtFeatureToggle(private val env: Map<String, String>) {
-    internal fun annullering() = env.getOrDefault("ANNULLERING", "false").toBoolean()
-}
