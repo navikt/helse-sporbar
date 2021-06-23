8f6052770d6757b4d608c4992987e22cb37eed4f
Hege Haavaldsen <hege.haavaldsen@nav.no>
2021-06-23 16:10:37 +0200

    Legger til ny tilstand for innhenting av skatteinntekter
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

--- src/main/kotlin/no/nav/helse/sporbar/VedtaksperiodeMediator.kt
+++ src/main/kotlin/no/nav/helse/sporbar/VedtaksperiodeMediator.kt
@@ -131,6 +131,7 @@ internal class VedtaksperiodeMediator(
             Tilstand.AVVENTER_ARBEIDSGIVERSØKNAD_UFERDIG_GAP,
             Tilstand.AVVENTER_UFERDIG_FORLENGELSE -> Situasjon.AvventerTidligerePeriode
             Tilstand.AVVENTER_VILKÅRSPRØVING,
+            Tilstand.AVVENTER_UTBETALINGSGRUNNLAG,
             Tilstand.AVVENTER_HISTORIKK,
             Tilstand.AVVENTER_ARBEIDSGIVERE,
             Tilstand.AVVENTER_ARBEIDSGIVERE_REVURDERING,
