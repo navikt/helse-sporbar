4b23163edab3ca9af32022f3a60204f5fef6d008
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-06-12 17:50:45 +0200

    JSON Schema for tbd.utbetaling
    * JsonSchemaValidator finner riktig schema og sjekker key + meldingstype i tillegg til å sjekke mot skjemaet.

--- src/test/kotlin/no/nav/helse/sporbar/JsonSchemaValidator.kt
+++ src/test/kotlin/no/nav/helse/sporbar/JsonSchemaValidator.kt
@@ -12,24 +12,36 @@ import org.junit.jupiter.api.Assertions.assertEquals
 
 internal object JsonSchemaValidator {
 
-    private val inntektsmeldingstatusSchema by lazy {
-        JsonSchemaFactory
-            .getInstance(SpecVersion.VersionFlag.V7)
-            .getSchema(InntektsmeldingStatusTest::class.java.getResource("/inntektsmelding/im-status-schema-1.0.0.json")!!.toURI())
-    }
+    private fun String.getSchema() = JsonSchemaFactory
+        .getInstance(SpecVersion.VersionFlag.V7)
+        .getSchema(InntektsmeldingStatusTest::class.java.getResource("/json-schema/tbd.$this.json")!!.toURI())
+
+    private val inntektsmeldingstatusSchema by lazy { "inntektsmeldingstatus".getSchema() }
+    private val vedtakSchema by lazy { "vedtak".getSchema() }
+    private val utbetalingSchema by lazy { "utbetaling".getSchema() }
+    private val annulleringSchema by lazy { "utbetaling__annullering".getSchema() }
 
     private fun JsonSchema.assertSchema(json: JsonNode) {
         val valideringsfeil = validate(json)
-        assertEquals(emptySet<ValidationMessage>(), valideringsfeil)
+        assertEquals(emptySet<ValidationMessage>(), valideringsfeil) { "${json.toPrettyString()}\n" }
     }
+
+    private fun Melding.hentSchema(): Triple<String, Meldingstype, JsonSchema> = when (topic) {
+        "tbd.inntektsmeldingstatus" -> Triple("sykmeldt", Meldingstype.Inntektsmeldingstatus, inntektsmeldingstatusSchema)
+        "tbd.vedtak" -> Triple("fødselsnummer", Meldingstype.Vedtak, vedtakSchema)
+        "tbd.utbetaling" -> when (json.path("event").asText()) {
+            "utbetaling_annullert" -> Triple("fødselsnummer", Meldingstype.Annullering, annulleringSchema)
+            "utbetaling_uten_utbetaling" -> Triple("fødselsnummer", Meldingstype.UtenUtbetaling, utbetalingSchema)
+            else -> Triple("fødselsnummer", Meldingstype.Utbetaling, utbetalingSchema)
+        }
+        else -> throw IllegalStateException("Mangler schema for topic $topic")
+    }.let { Triple(json.path(it.first).asText(), it.second, it.third) }
 
     internal fun Melding.validertJson(): JsonNode {
         if (!topic.startsWith("tbd.")) return json
-        val (fødselsnummerKey, schema) = when (topic) {
-            "tbd.inntektsmeldingstatus" -> "sykmeldt" to inntektsmeldingstatusSchema
-            else -> throw IllegalStateException("Mangler schema for topic $topic")
-        }
-        assertEquals(json.path(fødselsnummerKey).asText(), key) { "Meldinger skal publiseres med fødselsnummer som key." }
+        val (forventetFødselsnummer, forventetMeldingstype, schema) = hentSchema()
+        assertEquals(forventetFødselsnummer, key) { "Meldinger skal publiseres med fødselsnummer som key. Key=$key, Fødselsnummer=$forventetFødselsnummer" }
+        assertEquals(forventetMeldingstype, meldingstype)
         schema.assertSchema(json)
         return json
     }
