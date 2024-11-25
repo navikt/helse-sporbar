cee9834a4727246c44f881a8826b700111f04cae
david steinsland <david@davidsteinsland.net>
2024-11-25 10:29:14 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 val mainClass = "no.nav.helse.sporbar.AppKt"
 
 val rapidsAndRiversVersion = "2024112510241732526640.8542991368ca"
-val tbdLibsVersion = "2024.11.24-12.01-42fdc22d"
+val tbdLibsVersion = "2024.11.25-10.23-348b187b"
 val ktorVersion = "3.0.1"
 val junitJupiterVersion = "5.11.3"
 val mockkVersion = "1.13.13"
