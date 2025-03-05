17b6d179884bc5b379a42c34dc277db593e30bf4
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2025-03-05 09:21:50 +0100

    Bevarer versjon-bump tross revert a2ffc5d
    Det ble jo ikke noe av TilkommenV3, så f5dc043 ble meningsløs. Men litt rart å gå fra 2.0.2 tilbake til 2.0.1, så beholder 2.0.1

--- src/main/kotlin/no/nav/helse/sporbar/sis/Behandlingstatusmelding.kt
+++ src/main/kotlin/no/nav/helse/sporbar/sis/Behandlingstatusmelding.kt
@@ -13,7 +13,7 @@ data class Behandlingstatusmelding(
     val status: Behandlingstatustype,
     val eksterneSøknadIder: Set<UUID>
 ) {
-    val versjon = "2.0.1"
+    val versjon = "2.0.2"
 
     enum class Behandlingstatustype {
         OPPRETTET,
