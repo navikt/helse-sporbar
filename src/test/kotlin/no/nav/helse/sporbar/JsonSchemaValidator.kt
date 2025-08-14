74cc9411b77f3d39246931a43ae2252e529c87b9
Linus Johansson <linus.123@hotmail.com>
2025-08-14 09:36:54 +0200

    Gruppere meldingstyper etter hva de blir brukt til
    Co-authored-by: David Steinsland <david@davidsteinsland.net>

--- src/test/kotlin/no/nav/helse/sporbar/JsonSchemaValidator.kt
+++ src/test/kotlin/no/nav/helse/sporbar/JsonSchemaValidator.kt
@@ -18,7 +18,6 @@ internal object JsonSchemaValidator {
         .getInstance(SpecVersion.VersionFlag.V7)
         .getSchema(JsonSchemaValidator::class.java.getResource("/json-schema/tbd.$this.json")!!.toURI())
 
-    private val inntektsmeldingstatusSchema by lazy { "inntektsmeldingstatus".getSchema() }
     private val vedtakFattetSchema by lazy { "vedtak__fattet_arbeidstaker".getSchema() }
     private val vedtakFattetSelvstendigNæringsdrivendeSchema by lazy { "vedtak__fattet_selvstendig_næringsdrivende".getSchema() }
     private val vedtakAnnullertSchema by lazy { "vedtak__annullert".getSchema() }
@@ -31,17 +30,16 @@ internal object JsonSchemaValidator {
     }
 
     private fun Melding.hentSchema(): Pair<String, JsonSchema> = when (meldingstype) {
-        Meldingstype.VedtakFattet -> "fødselsnummer" to vedtakFattetSchema
-        Meldingstype.VedtakFattetSelvstendigNæringsdrivende -> "fødselsnummer" to vedtakFattetSelvstendigNæringsdrivendeSchema
-        Meldingstype.VedtakAnnullert -> "fødselsnummer" to vedtakAnnullertSchema
-        Meldingstype.Behandlingstilstand -> error("Mangler schema for meldingstype $meldingstype")
-        Meldingstype.Annullering -> "fødselsnummer" to annulleringSchema
-        Meldingstype.Utbetaling -> "fødselsnummer" to utbetalingSchema
-        Meldingstype.UtenUtbetaling -> "fødselsnummer" to utbetalingSchema
-        Meldingstype.Inntektsmeldingstatus -> "sykmeldt" to inntektsmeldingstatusSchema
+        "VedtakFattet" -> "fødselsnummer" to vedtakFattetSchema
+        "VedtakFattetSelvstendigNæringsdrivende" -> "fødselsnummer" to vedtakFattetSelvstendigNæringsdrivendeSchema
+        "VedtakAnnullert" -> "fødselsnummer" to vedtakAnnullertSchema
+        "Annullering" -> "fødselsnummer" to annulleringSchema
+        "Utbetaling" -> "fødselsnummer" to utbetalingSchema
+        "UtenUtbetaling" -> "fødselsnummer" to utbetalingSchema
+        else -> error("Mangler schema for meldingstype $meldingstype")
     }.let { json.path(it.first).asText() to it.second }
 
-    private fun Melding.udokumentertMelding() = (topic == "aapen-helse-sporbar" && meldingstype != Meldingstype.Annullering).also {
+    private fun Melding.udokumentertMelding() = (topic == "aapen-helse-sporbar" && meldingstype != "Annullering").also {
         if (it) {
             println("⚠️ Melding $meldingstype på $topic er ikke dokumentert, og blir ikke validert.")
         }
@@ -58,11 +56,11 @@ internal object JsonSchemaValidator {
     private fun Headers.meldingstypeOrNull() =
         map { it.key() to String(it.value()) }
             .singleOrNull { it.first == "type" }
-            ?.let { Meldingstype.valueOf(it.second) }
+            ?.second
 
     internal fun ProducerRecord<String, String>.validertJson() = Melding(
         topic = topic(),
-        meldingstype = headers().meldingstypeOrNull() ?: Meldingstype.VedtakFattet.also { require(topic() == "tbd.vedtak") },
+        meldingstype = headers().meldingstypeOrNull() ?: "VedtakFattet".also { require(topic() == "tbd.vedtak") },
         key = key(),
         json = mapper.readTree(value())
     ).validertJson()
@@ -70,7 +68,7 @@ internal object JsonSchemaValidator {
 
 class Melding(
     internal val topic: String,
-    internal val meldingstype: Meldingstype,
+    internal val meldingstype: String,
     internal val key: String,
     internal val json: JsonNode
 )
