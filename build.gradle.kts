7199a1c937eced152a34fb2ac4d36b2e3df3e7d4
david steinsland <david@davidsteinsland.net>
2025-08-16 14:03:45 +0200

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val mainClass = "no.nav.helse.sporbar.AppKt"
 
-val rapidsAndRiversVersion = "2025080710011754553680.051be9b54ef9"
+val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
 val ktorVersion = "3.2.3"
 val junitJupiterVersion = "5.12.1"
