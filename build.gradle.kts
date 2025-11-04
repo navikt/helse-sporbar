9cccc36d3c9f8a21a6289cc4bbbd5e07e49c179e
david steinsland <david@davidsteinsland.net>
2025-11-04 10:28:55 +0100

    bumper kotlin jvm, tbd libs og r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 val mainClass = "no.nav.helse.sporbar.AppKt"
 
-val rapidsAndRiversVersion = "2025092210081758528498.1d4b108f2c61"
-val tbdLibsVersion = "2025.09.19-15.24-1a9c113f"
+val rapidsAndRiversVersion = "2025110410191762247980.5e0592e08597"
+val tbdLibsVersion = "2025.11.04-10.12-3a167132"
 val ktorVersion = "3.2.3"
 val junitJupiterVersion = "5.12.1"
 val mockkVersion = "1.13.17"
@@ -9,7 +9,7 @@ val jsonSchemaValidatorVersion = "1.0.73"
 val jsonassertVersion = "1.5.1"
 
 plugins {
-    kotlin("jvm") version "2.2.10"
+    kotlin("jvm") version "2.2.21"
 }
 
 dependencies {
