9d7e3efabde8e173af58c9fa4b7fb0cbdc770e4d
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-06-30 12:25:06 +0200

    💚 Fiks repository-oppsettet og få CodeQL til å funke igjen
    Co-authored-by: Martin Remøy Solheim <3852471+martinmekk@users.noreply.github.com>
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>
    Co-authored-by: Sivert Lunsæter <sivert.frang.lunseter@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,23 +37,6 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-// Sett opp repositories basert på om vi kjører i CI eller ikke
-// Jf. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
-repositories {
-    mavenCentral()
-    if (providers.environmentVariable("GITHUB_ACTIONS").orNull == "true") {
-        maven {
-            url = uri("https://maven.pkg.github.com/navikt/maven-release")
-            credentials {
-                username = "token"
-                password = providers.environmentVariable("GITHUB_TOKEN").orNull!!
-            }
-        }
-    } else {
-        maven("https://repo.adeo.no/repository/github-package-registry-navikt/")
-    }
-}
-
 kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of("21"))
